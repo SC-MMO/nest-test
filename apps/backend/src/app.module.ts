fe1cdf22fa862ae409e4cf23600e4bde97f6ae9e
SC-MMO <moritz.mummert@scaltel.de>
2026-06-16 14:58:37 +0200

    start

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService} from './user.service.ts';
-import { PostsService} from './post.service.ts';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, UsersService, PostsService],
+  providers: [AppService],
 })
 export class AppModule {}
